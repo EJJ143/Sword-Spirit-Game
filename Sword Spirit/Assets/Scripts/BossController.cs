f4e8c099a6d955b2a3d6d045b309444846064691
Big-Boss-666V <127551079+Big-Boss-666V@users.noreply.github.com>
2023-03-25 11:31:00 -0700

    more coding

--- Sword Spirit/Assets/Scripts/BossController.cs
+++ Sword Spirit/Assets/Scripts/BossController.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class BossController : MonoBehaviour
 {
+    [SerializeField] private float xAxisInput;
+    [SerializeField] private float yAxisInput;
+    [SerializeField] private float speed = 10;
+
     // Start is called before the first frame update
     void Start()
     {
+        
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        xAxisInput = Input.GetAxis("Horizontal");
+        yAxisInput = Input.GetAxis("vertical");
+
+        transform.Translate(Vector3.right * speed * xAxisInput * Time.deltaTime);
+        transform.Translate(Vector3.up * speed * yAxisInput * Time.deltaTime);
     }
 }
