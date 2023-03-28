c92bba96b27450528e310830d3a98db6269fb601
Big-Boss-666V <127551079+Big-Boss-666V@users.noreply.github.com>
2023-03-27 20:40:34 -0700

    Added small movement on boss, he will move forward then fly up into space

--- Sword Spirit/Assets/Scripts/BossController.cs
+++ Sword Spirit/Assets/Scripts/BossController.cs
@@ -4,24 +4,49 @@ using UnityEngine;
 
 public class BossController : MonoBehaviour
 {
-   // [SerializeField] private float xAxisInput;
-   // [SerializeField] private float zAxisInput;
-   // [SerializeField] private float speed = 30;
-
-    // Start is called before the first frame update
+    [SerializeField] private float yAxisInput = 1;
+    [SerializeField] private float xAxisInput = -1;
+    [SerializeField] private float speed = 1;
+    [SerializeField] private int waitTime = 10;
+    [SerializeField] private int repeatTime = 20;
     void Start()
     {
-        
-        
+        StartCoroutine(moveBoss());       
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator moveBoss()
     {
-      //  xAxisInput = Input.GetAxis("Horizontal");
-        //zAxisInput = Input.GetAxis("Vertical");
+        yield return new WaitForSeconds(waitTime); // wait
+        
+        int count = 0;
+        while(count <= repeatTime)
+        {   
+            //transform.TransformDirection
+            transform.Translate(Vector3.right * speed * xAxisInput * Time.deltaTime);
+            
+            Debug.Log("pos " + transform.position);
+            Debug.Log("local pos " + transform.localPosition);
 
-        //transform.Translate(Vector3.forward * speed * xAxisInput * Time.deltaTime);
-        //transform.Translate(Vector3.right * speed * zAxisInput * Time.deltaTime);
+            count++;
+            
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(2); // wait 
+
+        int count2 = 0;
+        while (count2 <= repeatTime)
+        {
+            transform.Translate(Vector3.up * speed * yAxisInput * Time.deltaTime);
+           
+            Debug.Log("pos " + transform.position);
+            Debug.Log("local pos " + transform.localPosition);
+            
+            count2++;
+
+            yield return null;
+        }
+;
+        Destroy(gameObject);
     }
 }
