1619b3a1cb569868b17bc84334e68682fa101831
Big-Boss-666V <127551079+Big-Boss-666V@users.noreply.github.com>
2023-04-10 00:16:45 -0700

    I got the boss to correctly point in the charcter's direction

--- Sword Spirit/Assets/WalkBehavior.cs
+++ Sword Spirit/Assets/WalkBehavior.cs
@@ -33,16 +33,20 @@ public class WalkBehavior : StateMachineBehaviour
 
         speedOfLockOn = bossScript.lockOnSpeed;
         speedTowardTarget = bossScript.movementSpeed;
-
-        movement();
-
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        directionToFace = player.transform.position - boss.transform.position; // Vector that is created between two objects
+        Debug.DrawRay(boss.transform.position, directionToFace, Color.blue);
 
-    
+        desiredRotation = Quaternion.LookRotation(directionToFace); // Using base object's rotation to find rotation needed to match direction to face     
+
+        desiredRotation = Quaternion.Euler(0, desiredRotation.eulerAngles.y + 90f, 0); // Update desired rotation so the object only rotates in y axis to match direction, good in flat surfaces
+
+        boss.transform.rotation = Quaternion.Slerp(boss.transform.rotation, desiredRotation, speedOfLockOn * Time.deltaTime); // begun the actual process of rotating object
+
     }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
@@ -68,8 +72,5 @@ public class WalkBehavior : StateMachineBehaviour
         nextPosition = directionToFace * Time.deltaTime * speedTowardTarget;
 
         boss.transform.Translate(nextPosition);
-
-     
     }
-
 }
