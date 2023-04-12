9f765611284d9483eaddd4cd4694790d78aafad1
Big-Boss-666V <127551079+Big-Boss-666V@users.noreply.github.com>
2023-04-12 16:17:07 -0700

    Added sound to title screen and rules

--- Sword Spirit/Assets/Scripts/Animation Code/VictoryBehavior.cs
+++ Sword Spirit/Assets/Scripts/Animation Code/VictoryBehavior.cs
@@ -4,22 +4,25 @@ using UnityEngine;
 
 public class VictoryBehavior : StateMachineBehaviour
 {
+    private AudioSource playerCam;
+
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         animator.SetBool(Animator.StringToHash("extra"), false);
+        AudioSource playerCam = GameObject.FindGameObjectWithTag("MainCamera").transform.GetComponent<AudioSource>();
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
-    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        playerCam.volume = 0.03f;
+    }
 
-    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    //OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        playerCam.volume = 0;
+    }
 
 }
